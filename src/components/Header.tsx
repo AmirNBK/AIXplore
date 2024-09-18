5db63dd9d7e458cabf69d87823b57474ca51f8db
AmirHossein Norbakhsh <amircr7h@icloud.com>
2024-09-19 00:01:58 +0330

    developed the header

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '@/assets/icons/logo.svg'
+import Image from 'next/image';
+import localFont from 'next/font/local'
+const WorkSans = localFont({ src: '../assets/fonts/WorkSans-SemiBold.ttf' })
+const WorkSansRegular = localFont({ src: '../assets/fonts/WorkSans-Medium.ttf' })
+
+
+const Header = () => {
+    const headerItems = ['Home', 'AI Tools', 'About', 'Contact']
+    return (
+        <div className={`Header flex flex-row w-full justify-between items-center mt-6`}>
+            <Image src={logo} alt='logo' className='size-12' />
+
+            <div className={`${WorkSans.className} flex flex-row gap-12 text-[#161C2D]`}>
+                {headerItems.map((item, index) => {
+                    return (
+                        <p key={index}>
+                            {item}
+                        </p>
+                    )
+                })}
+            </div>
+
+            <button className={`${WorkSansRegular.className} bg-[#473BF0] py-2 px-4 rounded-lg text-white`}>
+                Get started free
+            </button>
+        </div>
+    );
+};
+
+export default Header;
